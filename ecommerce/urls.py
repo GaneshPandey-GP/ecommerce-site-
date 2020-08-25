1ab5288ab7d4cac90c7ac1c72b1fe3ef798fc42c
Tekraj Joshi <tekrajjoshi07@gmail.com>
2020-08-25 20:50:23 +0530

    Navbar added

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('api/', include('core.urls')),
 
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
